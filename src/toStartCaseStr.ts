d7abc0271caa6e33da656813e2dcbe52f3df4069
Norbert de Langen <ndelangen@me.com>
2023-02-03 15:48:31 +0100

    fix

--- src/toStartCaseStr.ts
+++ src/toStartCaseStr.ts
@@ -2,5 +2,6 @@ export function toStartCaseStr(str: string) {
   return str
     .replace(/_/g, ' ')
     .replace(/([a-z])([A-Z])/g, (str2, $1, $2) => `${$1} ${$2}`)
+    .replace(/([a-z])([0-9])/gi, (str2, $1, $2) => `${$1} ${$2}`)
     .replace(/(\s|^)(\w)/g, (str2, $1, $2) => $1 + $2.toUpperCase());
 }
