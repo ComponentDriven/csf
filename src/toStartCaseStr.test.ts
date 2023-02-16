bdd542407ecbae0df0321aed850dc96a28f0c98b
Norbert de Langen <ndelangen@me.com>
2023-02-16 15:39:15 +0100

    add more test, and fix an issue or 2

--- src/toStartCaseStr.test.ts
+++ src/toStartCaseStr.test.ts
@@ -1,20 +1,33 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable import/no-extraneous-dependencies */
+import startCase from 'lodash/startCase';
 import { toStartCaseStr } from './toStartCaseStr';
 
 test.each([
   ['snake_case', 'Snake Case'],
+  ['AAAaaaAAAaaa', 'AA Aaaa AA Aaaa'],
   ['kebab-case', 'Kebab Case'],
   ['camelCase', 'Camel Case'],
   ['camelCase1', 'Camel Case 1'],
-  ['camelCase1a', 'Camel Case 1a'],
-  ['camelCase1A', 'Camel Case 1A'],
-  ['camelCase1A2', 'Camel Case 1A 2'],
-  ['camelCase1A2b', 'Camel Case 1A 2b'],
-  ['camelCase1A2B', 'Camel Case 1A 2B'],
-  ['camelCase1A2B3', 'Camel Case 1A 2B 3'],
+  ['camelCase1a', 'Camel Case 1 A'],
+  ['camelCase1A', 'Camel Case 1 A'],
+  ['camelCase1A2', 'Camel Case 1 A 2'],
+  ['camelCase1A2b', 'Camel Case 1 A 2 B'],
+  ['camelCase1A2B', 'Camel Case 1 A 2 B'],
+  ['camelCase1A2B3', 'Camel Case 1 A 2 B 3'],
   ['__FOOBAR__', 'FOOBAR'],
   ['__FOO_BAR__', 'FOO BAR'],
+  ['__FOO__BAR__', 'FOO BAR'],
   [' FOO BAR', 'FOO BAR'],
-  ['1. Fooo', '1. Fooo'],
+  ['1. Fooo', '1 Fooo'],
+  ['ZIndex', 'Z Index'],
 ])('%s', (str, expected) => {
-  expect(toStartCaseStr(str)).toBe(expected);
+  const outcome = toStartCaseStr(str);
+  const fromLodash = startCase(str);
+
+  expect({ outcome, fromLodash }).toEqual({
+    outcome: expected,
+    fromLodash,
+  });
+  expect(outcome).toEqual(fromLodash);
 });
