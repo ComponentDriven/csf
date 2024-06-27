ba767850ebb52f8a3bf2bdafca6bc9c521dc750f
Kasper Peulen <kasperpeulen@gmail.com>
2024-06-27 11:12:54 +0200

    Disable unnecessary eslint rule

--- src/story.ts
+++ src/story.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 import type { RemoveIndexSignature, Simplify, UnionToIntersection } from 'type-fest';
 import type { SBScalarType, SBType } from './SBType';
 
